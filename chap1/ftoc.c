bf0d3c50784e0f7a1bb590a3979eba7c50726b1c
Jacob Gersztyn <jabocg@gmail.com>
2016-05-06 15:00:34 -0700

    Change from `int` to `float`

--- chap1/ftoc.c
+++ chap1/ftoc.c
@@ -4,8 +4,8 @@
  * for Fahrenheit 0, 20, ..., 300 */
 int main() 
 {
-    int fahr;
-    int cel;
+    float fahr;
+    float cel;
     int lower;
     int upper;
     int step;
@@ -16,8 +16,8 @@ int main()
 
     fahr = lower;
     while (fahr <= upper) {
-        cel = 5 * (fahr - 32) / 9;
-        printf("%3d\t%6d\n", fahr, cel);
+        cel = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, cel);
         fahr += step;
     }
 }
