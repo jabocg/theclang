993e2cfd1df3685a116ba0f8b9ba265db334cee7
Jacob Gersztyn <jabocg@gmail.com>
2016-05-06 14:42:26 -0700

    Add variables

--- chap1/ftoc.c
+++ chap1/ftoc.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/* print Fahrenheit to Celsius table
+ * for Fahrenheit 0, 20, ..., 300 */
 int main() {
-    
+    int fahr;
+    int cel;
+    int lower;
+    int upper;
+    int step;
 }
