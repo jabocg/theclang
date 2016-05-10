d98373905cf7b69cb6021a603c45d36cf810346c
Jacob Gersztyn <jabocg@gmail.com>
2016-05-10 14:38:02 -0700

    So this for loop works

--- chap1/ftocfor.c
+++ chap1/ftocfor.c
@@ -4,12 +4,8 @@
  * for Fahrenheit 0, 20, ..., 300 */
 int main() 
 {
-
-    printf(" F \t   C   \n");
-
-    int fahr;
-
-    for (fahr = 0; fahr < 300; fahr = fahr + 20) {
-        printf("%3.0f\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
+    for (int i = 0; i < 100; i++) {
+        printf("%d\n", i);
     }
+
 }
