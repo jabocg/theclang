ec3b0325f62bd43a2c4eba9e03b0a4017f7cd329
Jacob Gersztyn <jabocg@gmail.com>
2016-05-10 14:43:17 -0700

    Add table header

--- chap1/ftocfor.c
+++ chap1/ftocfor.c
@@ -4,6 +4,7 @@
  * for Fahrenheit 0, 20, ..., 300 */
 int main() 
 {
+    printf(" F \t   C   \n");
     for (int i = 0; i < 300; i += 20) {
         printf("%3d\t%6.1f\n", i, (5.0 / 9.0) * (i - 32.0));
     }
