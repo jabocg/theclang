92724cf939ec131b531f5210d1201430cc5ad53c
Jacob Gersztyn <jabocg@gmail.com>
2016-05-10 15:04:08 -0700

    Use symbolic constants

--- chap1/ftocfor.c
+++ chap1/ftocfor.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
 /* print Fahrenheit to Celsius table
  * for Fahrenheit 0, 20, ..., 300 */
 int main() 
 {
     printf(" F \t   C   \n");
-    for (int i = 300; i > 0; i -= 20) {
+    for (int i = UPPER; i > LOWER; i -= STEP) {
         printf("%3d\t%6.1f\n", i, (5.0 / 9.0) * (i - 32.0));
     }
 
