53c246455d1e99cd798ae959a2e3f65bc07ddbad
Jacob Gersztyn <jabocg@gmail.com>
2016-05-10 14:36:22 -0700

    Investigate why for loops are broken

--- chap1/ftocfor.c
+++ chap1/ftocfor.c
@@ -7,7 +7,9 @@ int main()
 
     printf(" F \t   C   \n");
 
-    for (int i = 0; i < 300; i += 20) {
-        printf("%3.0f\t%6.1f\n", i, (5.0 / 9.0) * (i - 32.0));
+    int fahr;
+
+    for (fahr = 0; fahr < 300; fahr = fahr + 20) {
+        printf("%3.0f\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
     }
 }
