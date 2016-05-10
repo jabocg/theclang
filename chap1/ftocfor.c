e71cb534d26a47c9c41a8afb8e2f6729b288e8c1
Jacob Gersztyn <jabocg@gmail.com>
2016-05-10 14:23:42 -0700

    Attempt to use for loop instead

---
+++ chap1/ftocfor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* print Fahrenheit to Celsius table
+ * for Fahrenheit 0, 20, ..., 300 */
+int main() 
+{
+
+    printf(" F \t   C   \n");
+
+    for (int i = 0; i < 300; i += 20) {
+        printf("%3.0f\t%6.1f\n", i, (5.0 / 9.0) * (i - 32.0));
+    }
+}
