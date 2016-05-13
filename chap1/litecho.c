366595232897263170d7d2410a938f4bebe03575
Jacob Gersztyn <jabocg@gmail.com>
2016-05-12 21:30:31 -0700

    Add escaping program
    It escapes tabs and backspaces(theoretically escapes backspaces, but how
            do you measure that?)

---
+++ chap1/litecho.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* echo input, using character literals for tabs(\t), backspaces(\b), and
+ * backslashes(\\) */
+
+int main() 
+{
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        } else if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+        } else if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+        } else {
+            putchar(c);
+        }
+    }
+}
